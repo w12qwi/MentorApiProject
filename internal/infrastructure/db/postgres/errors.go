43b4e81f44dd5c479fe960fce71ff88192bb8456
wi12qwi <wiqwiwiwiku@gmail.com>
2026-03-06 02:36:28 +0500

    deleted unimportant shit

--- internal/infrastructure/db/postgres/errors.go
+++
@@ -1,8 +0,0 @@
-package postgres
-
-import "errors"
-
-var (
-	UnableToConnectToPostgresError = errors.New("Unable to connect to postgres")
-	NoSuchCalculationError         = errors.New("No such calculation")
-)
